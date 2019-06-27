e4f0f18c96e541edafea3a86fd2a90407ba4e1b4
mcliamateo <51128802+mcliamateo@users.noreply.github.com>
2019-06-27 20:07:54 +0800

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::string name2;
+    std::string str;
+    int score1;
+    int score2;
+
+    std::getline(ifs,name1,';');
+    std::getline(ifs,name1,'\n');
+    score1=atoi(str.c_str());
+
+    std::getline(ifs,name2,';');
+    std::getline(ifs,name2,'\n');
+    score2=atoi(str.c_str());
+
+    return 0;
+}
