acd8e23526ed0116e2a71724355dbd8f3d34fdeb
Mayrien Joy Balilo <mjmbalilo@gmail.com>
2019-06-27 19:56:48 +0800

    Added Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,27 @@
+#include "Player.h"
+
+Player::Player()
+{
+    //ctor
+}
+
+Player::~Player()
+{
+    //dtor
+}
+void Player::setName(std::string n)
+{
+    name = n;
+}
+void Plater::setScore(int s)
+{
+    score = s;
+}
+std::string Plater::getName()
+{
+    return name;
+}
+int Player::getScore()
+{
+    return score;
+}
